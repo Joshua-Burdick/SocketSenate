1fc40564b068f916b3633d95b49a836e41c6294a
Joshua Burdick <joshburdick1@hotmail.com>
2023-12-06 00:25:39 -0500

    implementing threaded tcp clients

--- client/main.cpp
+++ client/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <cstring>
+#include <list>
 #include <stdexcept>
+#include <thread>
+#include <mutex>
+#include <chrono>
 #include "TCPClient.h"
 #include "UDPClient.h"
 
 #define PORT 8080
 #define MAXSIZE 1024
+#define HISTORY_SIZE 10
+
+std::mutex clientMtx, ioMtx;
+std::list<char[MAXSIZE]> messages(HISTORY_SIZE);
 
 void runTCPClient(TCPClient& client);
 void runUDPClient(UDPClient& client);
 
+void sendTCPMessage(TCPClient& client, bool& startupComplete);
+void sendUDPMessage(UDPClient& client);
+
+void readTCPResponse(TCPClient& client);
+void readUDPResponse(UDPClient& client);
+
 void setUsername(TCPClient& client);
 void setUsername(UDPClient& client);
+
+void render();
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -27,7 +43,11 @@ int main(int argc, char* argv[]) {
         client.connect(target);
         printf("\033[32mConnected to %s:%d\033[0m\n\n", target, port);
 
-        setUsername(client);
+        {
+            std::unique_lock<std::mutex> clientLock(clientMtx);
+            std::unique_lock<std::mutex> ioLock(ioMtx);
+            setUsername(client);
+        }
 
         runTCPClient(client);
     }
@@ -47,29 +67,19 @@ int main(int argc, char* argv[]) {
 }
 
 void runTCPClient(TCPClient& client) {
-    while (true) {
-        std::string buffer = "";
-        char response[MAXSIZE] = {0};
+    bool startupComplete = false;
+    std::thread sendThread(sendTCPMessage, std::ref(client), std::ref(startupComplete));
 
-        printf("Enter message: ");
-        std::cin.clear();
-        std::getline(std::cin, buffer);
-
-        client.send(buffer.c_str(), buffer.length());
-        printf("Message sent\n");
-
-        if (buffer == "exit") {
-            printf("Exiting...\n");
-            break;
-        }
-        else if (buffer == "shutdown") {
-            printf("Shutting down server...\n\n");
-            return;
-        }
-
-        client.read(response, MAXSIZE);
-        printf("Response: %s\n\n", response);
+    while (!startupComplete) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    std::thread readThread(readTCPResponse, std::ref(client));
+
+    // TODO - Renderer Thread
+    // Every x ms interval, clear console and rerender
+
+    sendThread.join();
+    readThread.detach();
 }
 
 void runUDPClient(UDPClient& client) {
@@ -95,6 +105,56 @@ void runUDPClient(UDPClient& client) {
 
         client.read(response, MAXSIZE);
         printf("Response: %s\n\n", response);
+    }
+}
+
+void sendTCPMessage(TCPClient& client, bool& startupComplete) {
+    while (true) {
+        std::string buffer = "";
+
+        { // TODO - Move to renderer thread
+            std::unique_lock<std::mutex> ioLock(ioMtx);
+            printf("Enter message: ");
+            std::cin.clear();
+            std::getline(std::cin, buffer);
+        }
+
+        {
+            std::unique_lock<std::mutex> clientLock(clientMtx);
+            if (!startupComplete) startupComplete = true;
+            client.send(buffer.c_str(), buffer.length());
+        }
+
+        {
+            // TODO - Move to renderer thread
+            // std::unique_lock<std::mutex> ioLock(ioMtx);
+            if (buffer == "exit") {
+                printf("Exiting...\n");
+                return;
+            }
+            else if (buffer == "shutdown") {
+                printf("Shutting down server...\n\n");
+                return;
+            }
+        }
+    }
+}
+
+void readTCPResponse(TCPClient& client) {
+    while (true) {
+        char response[MAXSIZE] = {0};
+
+        {
+            std::unique_lock<std::mutex> clientLock(clientMtx);
+            client.read(response, MAXSIZE);
+            messages.push_back(response);
+            messages.pop_front();
+        }
+
+        { // TODO - Move to renderer thread
+            std::unique_lock<std::mutex> ioLock(ioMtx);
+            printf("Response: %s\n\n", response);
+        }
     }
 }
 
@@ -138,4 +198,8 @@ void setUsername(UDPClient& client) {
     } while (true);
     
     printf("Username Set\n\n");
+}
+
+void render() {
+    // TODO - Clear console and render messages
 }
--- compile.sh
+++ compile.sh
@@ -5,4 +5,4 @@ cd server
 g++ -std=c++11 -o ../executables/socketServer main.cpp TCPServer.cpp UDPServer.cpp
 
 cd ../client
-g++ -std=c++11 -o ../executables/socketClient main.cpp TCPClient.cpp UDPClient.cpp
+g++ -std=c++11 -pthread -o ../executables/socketClient main.cpp TCPClient.cpp UDPClient.cpp
